88c96698df348af9b1d6137f47892191bf31a26f
Lisa Yan <yanlisa@berkeley.edu>
2025-11-20 13:22:29 -0800

    feat(lec21): gemini tutorial

--- 18-html/api_key.py
+++ 18-html/api_key.py
@@ -1,1 +1,4 @@
+# do NOT push your API key to a public repo.
+# Before committing to main, make sure the line below is
+# my_client_access_token = "MY_API_KEY"
 my_client_access_token = "MY_API_KEY"
